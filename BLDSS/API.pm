PTFS-Europe/koha-ill-bldss

commit 5a163af557b8a9f53915838303bf4a332fa4605d

Remove duplicate payCopyright parameter

BLDSS/API.pm
@@ -657,7 +657,7 @@ sub _encode_order {
 
   # Optional Parameters
   for my $name (
-    qw( requestor customerReference payCopyright allowWaitingList referrel))
+    qw( requestor customerReference allowWaitingList referrel))
   {
     if ($ref->{$name}) {
       $element = $doc->createElement($name);

commit 52a2d7629ad4bce80148fcb9dfeacc46aad95088

Fix regression introduced by 5a163af payCopyright would only duplicate if  exists. If  does not exist, we still need to consider payCopyright.

BLDSS/API.pm
@@ -662,7 +662,7 @@ sub _encode_order {
 
   # Optional Parameters
   for my $name (
-    qw( requestor customerReference allowWaitingList referrel))
+    qw( requestor customerReference payCopyright allowWaitingList referrel))
   {
     if ($ref->{$name}) {
       $element = $doc->createElement($name);